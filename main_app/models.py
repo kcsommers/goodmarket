43fe6f6d69a58ed7302ddd6321ce498bbe5caec0
Jacob Spade <jacobspade@Jacobs-MacBook-Pro.local>
2018-08-06 15:11:22 -0700

    adding models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class User(models.Model):
+    name = model.CharField(max_length=100)
+    email = model.CharField(max_length=100)
+    
