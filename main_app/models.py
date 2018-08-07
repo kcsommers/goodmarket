f031b92403ed095395711452933c6abedbb182a6
Mark Sommers <kacysommers@gmail.com>
2018-08-07 11:33:40 -0700

    css file

--- main_app/models.py
+++ main_app/models.py
@@ -11,15 +11,16 @@ class Profile(models.Model):
     picture = models.CharField(max_length=100)
     bio = models.CharField(max_length=2000)
     charity = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.profile.save()
 
 
 class Item(models.Model):
