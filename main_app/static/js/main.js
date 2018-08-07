226a494428091fdb26edc97d1f032183e7f05266
Mark Sommers <kacysommers@gmail.com>
2018-08-07 16:12:12 -0700

    more styling market

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
 	console.log('DOC READY');
-
+	
 	$('.category-filters button').click(function() {
+		let category = $(this).attr('data-id');
 		if($(this).is('.filter-active')) {
 			$(this).removeClass('filter-active');
+			$(`.${category}`).removeClass('unfiltered');
 		}
 		else {
 			$(this).addClass('filter-active');
+			$(`.${category}`).removeClass('filter').addClass('unfiltered');
+			$(`.market-item:not(.${category}, .unfiltered)`).addClass('filter');
 		}
 	});
 });
