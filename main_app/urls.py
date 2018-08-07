fe6b2bb8a797aa82b0bdeada976c1a9d5116c91b
Jacob Spade <jacobspade@Jacobs-MacBook-Pro.local>
2018-08-07 10:42:43 -0700

    working on show and charity views

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	path('login/', views.login_view, name="login"),
 	path('logout/', views.logout_view, name="logout"),
-	path('checkout/', views.checkout, name="checkout")
+	path('checkout/', views.checkout, name="checkout"),
+	path('show/', views.show_view, name="show"),
+	path('charity/', views.charity_view, name="charity")
 ]
