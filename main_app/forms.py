5385df84636aac22310198afe37cd18554c4cfd9
Mark Sommers <kacysommers@gmail.com>
2018-08-10 11:33:09 -0700

    login form

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Item, Profile, Cart
 
@@ -7,25 +6,42 @@ class LoginForm(forms.Form):
 	username = forms.CharField(label="Username", max_length=64, widget=forms.TextInput(attrs={'placeholder':'Username'}))
 	password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
 
-class SignupForm(UserCreationForm):
-	first_name = forms.CharField(max_length=30, required=False)
-	last_name = forms.CharField(max_length=30, required=False)
-	email = forms.EmailField(max_length=254)
+class SignUpForm(forms.ModelForm):
+	    """
+	    A form that creates a user, with no privileges, from the given username and
+	    password.
+	    """
+	    error_messages = {
+	        'password_mismatch': ("The two password fields didn't match."),
+	    }
+	    username = forms.CharField(label=("Username"), widget=forms.TextInput(attrs={'class': 'signup-username', 'placeholder': 'Username'}))
+	    password1 = forms.CharField(label=("Password"),
+	        widget=forms.PasswordInput(attrs={'class': 'signup-p1', 'placeholder': 'Password'}))
+	    password2 = forms.CharField(label=("Password confirmation"),
+	        widget=forms.PasswordInput(attrs={'class': 'signup-p1', 'placeholder': 'Confirm Password'}),
+	        help_text=("Enter the same password as above, for verification."))
 
-	class Meta:
-			model = User
-			fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
-	
-	def __init__(self):
-		super(UserCreationForm, self).__init__()
-		self.fields['username'].widget.attrs['placeholder'] = 'Username'
-		self.fields['first_name'].widget.attrs['placeholder'] = 'First Name'
-		self.fields['last_name'].widget.attrs['placeholder'] = 'Last Name'
-		self.fields['email'].widget.attrs['placeholder'] = 'Email'
-		self.fields['password1'].widget.attrs['placeholder'] = 'Password'
-		self.fields['password2'].widget.attrs['placeholder'] = 'Password Confirmation'
-		for fieldname in ['username', 'password1', 'password2']:
-			self.fields[fieldname].help_text = None
+	    class Meta:
+	        model = User
+	        fields = ("username", 'password1', 'password2')
+
+	    def clean_password2(self):
+	        password1 = self.cleaned_data.get("password1")
+	        password2 = self.cleaned_data.get("password2")
+	        if password1 and password2 and password1 != password2:
+	            raise forms.ValidationError(
+	                self.error_messages['password_mismatch'],
+	                code='password_mismatch',
+	            )
+	        return password2
+
+	    def save(self, commit=True):
+	        user = super(SignUpForm, self).save(commit=False)
+	        user.set_password(self.cleaned_data["password1"])
+	        if commit:
+	            user.save()
+
+	        return user
 
 class SellForm(forms.ModelForm):
 	image = forms.ImageField()
