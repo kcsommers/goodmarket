6554a3a55d2883226a6f49f257268f8aff1a70a8
Mark Sommers <kacysommers@gmail.com>
2018-08-11 14:37:54 -0700

    handle checkout data

--- main_app/models.py
+++ main_app/models.py
@@ -10,20 +10,29 @@ class Profile(models.Model):
     image = models.ImageField(upload_to='profile_images', blank=True)
     bio = models.CharField(max_length=2000)
     charity = models.IntegerField(default=0)
-    stripe_user_id = models.CharField(max_length=200, default='')
+    stripe_user_id = models.CharField(max_length=200, null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.user.username
+
+class Charity(models.Model):
+    total_money_raised = models.IntegerField(default=0)
+    name = models.CharField(max_length=100)
+    mission_statement = models.CharField(max_length=1000)
+    image = models.CharField(max_length=1000)
+    link = models.CharField(max_length=1000)
+    def __str__(self):
+        return self.name
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='item_images', blank=True)
     category = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
-    charity_percent = models.IntegerField(default=0)
-    charity = models.CharField(max_length=100)
     price = models.IntegerField(default=0)
     sold = models.BooleanField(default=False)
+    charity_percent = models.IntegerField(default=0)
+    charity = models.ForeignKey(Charity, null=True, on_delete=models.SET_NULL)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
@@ -31,8 +40,3 @@ class Item(models.Model):
 class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     items = models.ManyToManyField(Item)
-
-class Charity(models.Model):
-    total_money_raised = models.IntegerField(default=0)
-    name = models.CharField(max_length=100)
-    mission_statement = models.CharField(max_length=1000)
